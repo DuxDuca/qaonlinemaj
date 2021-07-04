db45a9e75f573fa9568382e6a00c35ea44e8d5c8
Dusan Ristic <duxshanda@gmail.com>
2021-07-04 18:07:28 +0200

    Klasa koja demonstrira thread sleep

---
+++ java-intro/src/basics/ThreadSleepDemo.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class ThreadSleepDemo {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("sad ce biti 5 secundi izmedju ispisa");
+		Thread.sleep(5000);
+		System.out.println("jesam vam rekao");
+	}
+
+}
